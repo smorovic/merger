9418c80d02187f85c0b3d649831434d4aef67b89
Jan Veverka <jan.veverka@gmail.com>
2014-04-13 14:18:53 -0600

    Added a tool to help with calculating the average throughput and started a list of useful one-liners

---
+++ hwtest/useful_oneliners.sh
@@ -0,0 +1,15 @@
+## Launch a test (on node 10 in /root/merger/hwtest)
+. launch.sh
+
+## Watch the input and output folders on lustre to be
+## deleted, created, and filled (on node 10)
+watch "du -sk /lustre/testHW/*mev/null"
+
+## Watch a merger start up (and get killed, on nodes 13, 14)
+watch "echo $HOSTNAME; ps awwx | grep doMerging | grep -v grep"
+
+## Watch producers start up, spawn sub-processes, and close (on nodes 1, 2)
+watch "echo $HOSTNAME; ps awwx | grep manageStreams | grep -v grep"
+
+## Check that ther are no input files left (on node 10)
+ls /lustre/testHW/unmerged*/*
