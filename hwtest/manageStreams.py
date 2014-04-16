3de667397c5787b2471ec5c0760355c2bab639a9
Jan Veverka <jan.veverka@gmail.com>
2014-04-16 13:16:42 +0200

    Working on fixing the customization of LS frequency. This version is still broken...

--- hwtest/manageStreams.py
+++ hwtest/manageStreams.py
@@ -10,49 +10,12 @@ import multiprocessing
 import time,datetime
 import random
 import math
-# git commit -a -m "Can customize the length of lumisections and randomize the producer time
 
-mean_seconds_per_lumi = 85.7
-## Set to a very small positive number to avoid the random component
-sigma_seconds_per_lumi = math.sqrt(mean_seconds_per_lumi)
 
-def configureStreams(fileName):
-    streamsConfigFile = fileName 
-
-    try:
-        if os.path.isfile(streamsConfigFile):
-            config = ConfigObj(streamsConfigFile)
-        else:
-            print "Configuration file not found: {0}!".format(streamsConfigFile)
-            sys.exit(1)
-    except IOError, e:
-        print "Unable to open configuration file: {0}!".format(streamsConfigFile)
-        sys.exit(1)
-    
-    return config
-
-def startCreateFiles (streamName, contentInputFile, lumiSections, runNumber, theBUNumber, thePath):
-          createFiles(streamName, contentInputFile, lumiSections, runNumber, theBUNumber, thePath)
-
-if __name__ == '__main__':
-
-    parser = OptionParser(usage="usage: %prog [-h|--help] -c|--config config -b|--bu BUNumber | -p|--path Path | -i|--inputPath inputPath")
-
-    parser.add_option("-c", "--config",
-                      action="store", dest="configFile",
-                      help="Configuration file storing the info related to the simulated streams of data. Absolute path is needed")
-
-    parser.add_option("-b", "--bu",
-                      action="store", dest="BUNumber",
-                      help="BU number")
-
-    parser.add_option("-p", "--path",
-                      action="store", dest="Path",
-                      help="Path to make")
-
-    parser.add_option("-i", "--inputPath",
-                      action="store", dest="inputPath",
-                      help="Input path")
+#______________________________________________________________________________
+def main():
+    print "DEBUG: Starting ..."
+    parser = make_option_parser()
 
     options, args = parser.parse_args()
 
@@ -78,6 +41,14 @@ if __name__ == '__main__':
        msg = "BIG PROBLEM, file does not exists!: %s" % str(theInputPath)
        raise RuntimeError, msg
 
+    mean_lumi_length = 20.
+    if (options.mean_lumi_length != None):
+       mean_lumi_length = options.mean_lumi_length
+    
+    sigma_lumi_length = 0.001
+    if (options.sigma_lumi_length != None):
+       sigma_lumi_length = options.sigma_lumi_length
+    
     params = configureStreams(options.configFile)
     filesNb = params['Streams']['number']
     lumiSections = int(params['Streams']['ls'])
@@ -96,12 +67,14 @@ if __name__ == '__main__':
            contentInputFile.append(theInputfile.read())
         theInputfile.close()
 
+    print "DEBUG: Looping over lumis ..."
     for ls in range(lumiSections): 
        processs = []
        
-       # Produce files every mean_seconds_per_lumi seconds with a random flutuation
-       time.sleep(seconds_to_wait(mean_seconds_per_lumi, 
-                                  sigma_seconds_per_lumi))
+       # Produce files every mean_lumi_length seconds with a random flutuation
+       sleep_time = seconds_to_wait(mean_lumi_length, sigma_lumi_length)
+       print "DEBUG: sleep time:", sleep_time
+       time.sleep(sleep_time)
           
        now = datetime.datetime.now()
    
@@ -115,11 +88,66 @@ if __name__ == '__main__':
        time.sleep(1)
     now = datetime.datetime.now()
     print now.strftime("%H:%M:%S"), ": finished, exiting..."
+## main
+    
+#______________________________________________________________________________
+def make_option_parser():
+    parser = OptionParser(usage="usage: %prog [-h|--help] -c|--config config -b|--bu BUNumber | -p|--path Path | -i|--inputPath inputPath")
 
+    parser.add_option("-c", "--config",
+                      action="store", dest="configFile",
+                      help="Configuration file storing the info related to the simulated streams of data. Absolute path is needed")
+
+    parser.add_option("-b", "--bu",
+                      action="store", dest="BUNumber",
+                      help="BU number")
+
+    parser.add_option("-p", "--path",
+                      action="store", dest="Path",
+                      help="Path to make")
+
+    parser.add_option("-i", "--inputPath",
+                      action="store", dest="inputPath",
+                      help="Input path")
+
+    parser.add_option("-m", "--mean-lumi-length",
+                      action="store", dest="mean_lumi_length",
+                      help="Mean lenght of lumi sections in seconds as a float")
+
+    parser.add_option("-s", "--sigma-lumi-length",
+                      action="store", dest="sigma_lumi_length",
+                      help=("Standard deviation of lumi section length " +
+                            "distribution as a float"))
+    return parser
+## make_option_parser
+
+
+#______________________________________________________________________________
+def configureStreams(fileName):
+    streamsConfigFile = fileName 
+
+    try:
+        if os.path.isfile(streamsConfigFile):
+            config = ConfigObj(streamsConfigFile)
+        else:
+            print "Configuration file not found: {0}!".format(streamsConfigFile)
+            sys.exit(1)
+    except IOError, e:
+        print "Unable to open configuration file: {0}!".format(streamsConfigFile)
+        sys.exit(1)
     
-#______________________________________________________________________________    
-def seconds_to_wait(mean_seconds_per_lumi=20, sigma_seconds_per_lumi=0.001):
-    return random.lognormalvariate(mean_seconds_per_lumi, 
-                                   sigma_seconds_per_lumi)
+    return config
+
+#______________________________________________________________________________
+def seconds_to_wait(mean_lumi_length=20, sigma_lumi_length=0.001):
+    return random.lognormvariate(math.log(mean_lumi_length), sigma_lumi_length)
 ## seconds_to_wait
-    
+
+#______________________________________________________________________________
+def startCreateFiles (streamName, contentInputFile, lumiSections, runNumber, theBUNumber, thePath):
+          createFiles(streamName, contentInputFile, lumiSections, runNumber, theBUNumber, thePath)
+
+
+#______________________________________________________________________________
+if __name__ == '__main__':
+    main()
