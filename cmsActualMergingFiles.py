9521e0c8c4bc3298725fce1f627c9a43d77a85cf
Guillelmo Gomez-Ceballos <ceballos@cern.ch>
2015-02-19 19:46:31 +0100

    new

--- cmsActualMergingFiles.py
+++ cmsActualMergingFiles.py
@@ -635,13 +635,3 @@ def append_files(ifnames, ofile):
                 shutil.copyfileobj(ifile, ofile)
             ifile.close()
 # append_files
-
-#______________________________________________________________________________
-def copyfileobj(fsrc, fdst, length=16*1024):
-    """copy data from file-like object fsrc to file-like object fdst"""
-    while 1:
-        buf = fsrc.read(length)
-        if not buf:
-            break
-        fdst.write(buf)
-# copyfileobj
