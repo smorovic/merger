d60aeffc39afd53c7e609f2aeffd41cc6bb1f152
Guillelmo Gomez-Ceballos <ceballos@cern.ch>
2015-03-11 21:17:00 +0100

    minor changes

---
+++ makeTarballProd.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+tar cvzf tarballMerger.tgz \
+    cmsActualMergingFiles.py \
+    cmsDataFlowCleanUp.py \
+    cmsDataFlowMakeFolders.py \
+    cmsDataFlowMerger.py \
+    configobj.py \
+    Daemon.py \
+    dataFlowMerger \
+    logFormat.conf \
+    Logging.py \
+    zlibextras.so \
+    dataFlowMerger.conf
