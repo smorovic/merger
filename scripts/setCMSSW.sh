77172df94c7e6b17d173f40c05995c72ff31be3e
Guillelmo Gomez-Ceballos <ceballos@cern.ch>
2015-10-06 21:52:04 +0200

    new fix

--- scripts/setCMSSW.sh
+++ scripts/setCMSSW.sh
@@ -4,7 +4,7 @@ myDir=$PWD/;
 
 cd /opt/offline/;
 source $PWD/cmsset_default.sh;
-cd slc6_amd64_gcc481/cms/cmssw/CMSSW_7_3_2/;
+cd slc6_amd64_gcc491/cms/cmssw/CMSSW_7_4_14/;
 eval `scramv1 runtime -sh`;
 
 cd $myDir;
